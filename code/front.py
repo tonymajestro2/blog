b51834bbfa4587ae4d9dd95d12e01a4f5999e316
Tony Majestro <tmajest@vt.edu>
2013-02-05 18:38:19 -0500

    Fixed bug where public blogs not displaying correctly

--- code/front.py
+++ code/front.py
@@ -1,13 +1,13 @@
-from base import BaseHandler, RestrictedToLoginHandler
+from base import RestrictedToLoginHandler
 from models import User, Post
 
 class Front(RestrictedToLoginHandler):
-    def render(self, **params):
-        posts = self.user.posts.order("-created").run(limit = 10)
+    def render(self, user, **params):
+        posts = user.posts.order("-created").run(limit = 10)
         posts_html = self.render_posts(posts)
         params["posts"] = posts_html
-        params["username"] = self.user.username
-        super(Front, self).render("front.html", self.user.username, **params)
+        params["username"] = user.username
+        super(Front, self).render("front.html", user.username, **params)
 
     def render_posts(self, posts):
         html_list = []
@@ -22,6 +22,8 @@ class Front(RestrictedToLoginHandler):
         self.render()
 
 class PublicFront(Front):
+    def render(self, user, **params):
+        super(PublicFront, self).render(user, **params)
 
     def get(self, username):
         user = User.get_by_name(username)
@@ -29,10 +31,13 @@ class PublicFront(Front):
             self.error(404)
             return
 
-        self.render(user = user)
+        self.render(user)
 
 
 class PrivateFront(Front):
+    def render(self, **params):
+        super(PrivateFront, self).render(self.user, **params)
+
     def get(self):
         create_button = self.get_html("front_button.html")
         self.render(createButton = create_button)
