6e8b0cde9879ac74c29520b8a2dde9151ae7f9c5
Tony Majestro <tmajest@vt.edu>
2013-02-09 18:58:48 -0500

    Cleaned up public/private pages html and css

--- code/front.py
+++ code/front.py
@@ -2,42 +2,33 @@ from base import RestrictedToLoginHandler
 from models import User, Post
 
 class Front(RestrictedToLoginHandler):
-    def render(self, user, **params):
-        posts = user.posts.order("-created").run(limit = 10)
-        posts_html = self.render_posts(posts)
-        params["posts"] = posts_html
-        params["username"] = user.username
-        super(Front, self).render("front.html", user.username, **params)
+    def render(self, user, page, **params):
+        params["s"] = self
+        super(Front, self).render(page, user.username, **params)
 
-    def render_posts(self, posts):
-        html_list = []
-        for p in posts:
-            params = dict(title = p.title, created = p.created, body = p.body)
-            html = self.get_html("post.html", **params)
-            html_list.append(html)
+    def render_post(self, p):
+        params = dict(title = p.title, created = p.created, body = p.body)
+        return self.get_html("post.html", **params)
 
-        return "".join(html_list)
+    def render_front(self):
+        posts = self.user.posts.order("-created").run(limit = 10)
+        params = dict(posts = posts, username = self.user.username, s = self)
+        return self.get_html("front.html", **params)
 
     def get(self):
         self.render()
 
-class PublicFront(Front):
-    def render(self, user, **params):
-        super(PublicFront, self).render(user, **params)
 
+class PublicFront(Front):
     def get(self, username):
         user = User.get_by_name(username)
         if not (user and user.public):
             self.error(404)
             return
 
-        self.render(user)
+        self.render(user, "front_public.html")
 
 
 class PrivateFront(Front):
-    def render(self, **params):
-        super(PrivateFront, self).render(self.user, **params)
-
     def get(self):
-        create_button = self.get_html("front_button.html")
-        self.render(createButton = create_button)
+        self.render(self.user, "front_private.html")
