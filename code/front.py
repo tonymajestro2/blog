92238277b7bde3dd4b3db8dfb6a2b201e57c0f26
Tony Majestro <tmajest@vt.edu>
2013-02-09 20:33:28 -0500

    Made public blogs accessible when logged out

--- code/front.py
+++ code/front.py
@@ -1,10 +1,10 @@
-from base import RestrictedToLoginHandler
+from base import RestrictedToLoginHandler, BaseHandler
 from models import User, Post
 
-class Front(RestrictedToLoginHandler):
-    def render(self, user, page, **params):
+class PrivateFront(RestrictedToLoginHandler):
+    def render(self, **params):
         params["s"] = self
-        super(Front, self).render(page, user.username, **params)
+        super(PrivateFront, self).render("front_private.html", self.user.username, **params)
 
     def render_post(self, p):
         params = dict(title = p.title, created = p.created, body = p.body)
@@ -19,16 +19,26 @@ class Front(RestrictedToLoginHandler):
         self.render()
 
 
-class PublicFront(Front):
+class PublicFront(BaseHandler):
+    def render(self, user, **params):
+        params["s"] = self
+        params["user"] = user
+        super(PublicFront, self).render("front_public.html", user.username, **params)
+
     def get(self, username):
         user = User.get_by_name(username)
         if not (user and user.public):
             self.error(404)
             return
 
-        self.render(user, "front_public.html")
+        self.render(user)
 
+    def render_post(self, p):
+        params = dict(title = p.title, created = p.created, body = p.body)
+        return self.get_html("post.html", **params)
 
-class PrivateFront(Front):
-    def get(self):
-        self.render(self.user, "front_private.html")
+    def render_front(self, user):
+        posts = user.posts.order("-created").run(limit = 10)
+        params = dict(posts = posts, username = user.username, s = self)
+        return self.get_html("front.html", **params)
+
